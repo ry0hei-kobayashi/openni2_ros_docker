cb3334901bd6264a8e1d3dc85556986562682df2
ry0hei-kobayashi <kobayashi.ryohei621@mail.kyutech.jp>
2024-11-01 14:26:56 +0800

    add interface&service

---
+++ openni2_service/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+
+package_name = 'openni2_service'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    package_dir={'': '.'},
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ry0hei-kobayashi',
+    maintainer_email='kobayashi.ryohei621@mail.kyutech.jp',
+    license='Apache Licence 2.0',
+    entry_points={
+        'console_scripts': [
+        'xtion_service_server = openni2_service.xtion_service_server:main',
+        'xtion_service_client = openni2_service.xtion_service_client:main',
+        ],
+    },
+)
